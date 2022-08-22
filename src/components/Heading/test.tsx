281c3b661f8102886562af1aef825335fcc46db4
Renato D. Schneider <renato@brasilfone.com.br>
2022-08-22 13:56:56 -0300

    feat: adicionado o heading ao projeto

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -17,4 +17,22 @@ describe('<Heading />', () => {
       color: '#030517'
     })
   })
+
+  it('should render a heading with a line to the left side', () => {
+    renderWithTheme(<Heading lineLeft>Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      'border-left': '0.7rem solid #3CD3C1'
+    })
+  })
+
+  it('should render a heading with a line at the bottom', () => {
+    renderWithTheme(<Heading lineBottom>Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
+      'border-bottom',
+      '0.5rem solid #F231A5',
+      {
+        modifier: '::after'
+      }
+    )
+  })
 })
