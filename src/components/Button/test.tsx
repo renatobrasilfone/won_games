564364a147e9141e6f38a60e930e8aeb9bac136f
Renato D. Schneider <renato@brasilfone.com.br>
2022-08-23 09:46:58 -0300

    test: finalizado testes de estilos do button

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -1,9 +1,56 @@
-import { render } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+import { AddShoppingCart } from '@styled-icons/material/AddShoppingCart'
 
 import Button from '.'
 
 describe('<Button />', () => {
-  it('should render the heading', () => {
-    render(<Button />)
+  it('should render the medium button size', () => {
+    const { container } = renderWithTheme(<Button>Buy now</Button>)
+
+    expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
+      height: '4rem',
+      padding: '0.8rem 3.2rem',
+      'font-size': '1.4rem'
+    })
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the button in small size', () => {
+    renderWithTheme(<Button size="small">Buy now</Button>)
+
+    expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
+      height: '3rem',
+      padding: '0.8rem',
+      'font-size': '1.2rem'
+    })
+  })
+
+  it('should render the button in large size', () => {
+    renderWithTheme(<Button size="large">Buy now</Button>)
+
+    expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
+      height: '5rem',
+      padding: '0.8rem 4.8rem',
+      'font-size': '1.6rem'
+    })
+  })
+
+  it('should render the button at maximum size', () => {
+    renderWithTheme(<Button fullSize>Buy now</Button>)
+
+    expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyle({
+      width: '100%'
+    })
+  })
+
+  it('should render the version of the button with icon', () => {
+    renderWithTheme(
+      <Button icon={<AddShoppingCart data-testid="icon" />}>Buy now</Button>
+    )
+
+    expect(screen.getByText(/buy now/i)).toBeInTheDocument()
+    expect(screen.getByTestId('icon')).toBeInTheDocument()
   })
 })
