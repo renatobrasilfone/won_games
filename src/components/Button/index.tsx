0cb8ec2b4f9c3694a85c100ae3149a15d626fcfe
Renato D. Schneider <renato@brasilfone.com.br>
2022-08-23 09:46:30 -0300

    style: estilizado componente button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,25 @@
+import React from 'react'
 import * as S from './styles'
 
 export type ButtonProps = {
   children?: React.ReactNode
   size?: 'small' | 'medium' | 'large'
+  fullSize?: boolean
+  icon?: React.ReactElement
+  onClick?: () => (event: React.MouseEvent<HTMLButtonElement>) => void
 }
 
-const Button = ({ children, size }: ButtonProps) => (
-  <S.Wrapper size={size}>{!!children && <span>{children}</span>}</S.Wrapper>
+const Button = ({
+  children,
+  size = 'medium',
+  fullSize = false,
+  icon,
+  ...props
+}: ButtonProps) => (
+  <S.Wrapper size={size} fullSize={fullSize} hasIcon={!!icon} {...props}>
+    {!!icon && icon}
+    {!!children && <span>{children}</span>}
+  </S.Wrapper>
 )
 
 export default Button
--- src/components/Button/stories.tsx
+++ src/components/Button/stories.tsx
@@ -1,5 +1,6 @@
 import { Story, Meta } from '@storybook/react'
 import Button, { ButtonProps } from '.'
+import { AddShoppingCart } from '@styled-icons/material/AddShoppingCart'
 
 export default {
   title: 'Button',
@@ -15,4 +16,12 @@ export const Default: Story<ButtonProps> = (args) => <Button {...args} />
 
 Default.args = {
   children: 'Buy Now'
+}
+
+export const withIcon: Story<ButtonProps> = (args) => <Button {...args} />
+
+withIcon.args = {
+  size: 'small',
+  children: 'Buy Now',
+  icon: <AddShoppingCart />
 }
